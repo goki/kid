f6ec881d9a99eef04be692aabbb8fc1ccb51a21d
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-16 08:00:41 -0800

    added standalone Auth

--- auth.go
+++ auth.go
@@ -3,3 +3,65 @@
 // license that can be found in the LICENSE file.
 
 package kid
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"net/http"
+
+	"github.com/coreos/go-oidc/v3/oidc"
+	"goki.dev/goosi"
+	"golang.org/x/oauth2"
+)
+
+// Auth authenticates the user with the given platform and returns the
+// resulting oauth token and user info.
+func Auth(ctx context.Context, issuerName string, issuerURL string) (*oauth2.Token, *oidc.UserInfo, error) {
+	provider, err := oidc.NewProvider(ctx, issuerURL)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	config := oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		RedirectURL:  "http://127.0.0.1:5556/auth/" + issuerName + "/callback",
+		Endpoint:     provider.Endpoint(),
+		Scopes:       []string{oidc.ScopeOpenID, "profile", "email"},
+	}
+	fmt.Println(config)
+
+	b := make([]byte, 16)
+	rand.Read(b)
+	state := base64.RawURLEncoding.EncodeToString(b)
+
+	code := make(chan string)
+
+	sm := http.NewServeMux()
+	sm.HandleFunc("/auth/"+issuerName+"/callback", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Query().Get("state") != state {
+			http.Error(w, "state did not match", http.StatusBadRequest)
+			return
+		}
+		code <- r.URL.Query().Get("code")
+		w.Write([]byte("<h1>Signed in</h1><p>You can return to the app</p>"))
+	})
+	// TODO(kai/auth): more graceful closing / error handling
+	go http.ListenAndServe("127.0.0.1:5556", sm)
+
+	goosi.TheApp.OpenURL(config.AuthCodeURL(state))
+
+	cs := <-code
+
+	oauth2Token, err := config.Exchange(ctx, cs)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to exchange token: %w", err)
+	}
+	userInfo, err := provider.UserInfo(ctx, oauth2.StaticTokenSource(oauth2Token))
+	if err != nil {
+		return oauth2Token, nil, fmt.Errorf("failed to get user info: %w", err)
+	}
+	return oauth2Token, userInfo, nil
+}
