3c7c1671d252f02c5113f1533d7678ea048d01ca
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-21 09:13:11 -0800

    added scopes example

---
+++ examples/scopes/scopes.go
@@ -0,0 +1,37 @@
+// Copyright (c) 2023, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"github.com/coreos/go-oidc/v3/oidc"
+	"goki.dev/gi/v2/gi"
+	"goki.dev/gi/v2/gimain"
+	"goki.dev/gi/v2/giv"
+	"goki.dev/grr"
+	"goki.dev/kid"
+	"golang.org/x/oauth2"
+)
+
+func main() { gimain.Run(app) }
+
+func app() {
+	gi.SetAppName("kid-scopes")
+	b := gi.NewBody().SetTitle("Kid Scopes Example")
+	fun := func(token *oauth2.Token, userInfo *oidc.UserInfo) {
+		d := gi.NewBody().AddTitle("User info")
+		gi.NewLabel(d).SetType(gi.LabelHeadlineMedium).SetText("Basic info")
+		giv.NewStructView(d).SetStruct(userInfo)
+		gi.NewLabel(d).SetType(gi.LabelHeadlineMedium).SetText("Detailed info")
+		claims := map[string]any{}
+		grr.Log0(userInfo.Claims(&claims))
+		giv.NewMapView(d).SetMap(&claims)
+		d.AddBottomBar(func(pw gi.Widget) {
+			d.AddOk(pw)
+		})
+		d.NewFullDialog(b).Run()
+	}
+	kid.Buttons(b, fun, "https://mail.google.com/") // <- this is where the scopes go
+	b.NewWindow().Run().Wait()
+}
