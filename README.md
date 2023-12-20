d5b286865fcea642d36942493284bb7e31486103
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-20 15:54:59 -0800

    updated version to v0.0.10

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # kid
-Kid (Ki ID) provides a system for identifying and authenticating users through third party cloud systems in GoKi apps.
+Kid (Ki ID) provides a system for identifying and authenticating users through third party cloud systems in Goki apps.
--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package kid
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.0.9"
+	Version = "v0.0.10"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "86269bb"
+	GitCommit = "2aa54b6"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-13 01:06"
+	VersionDate = "2023-12-20 23:54"
 )
